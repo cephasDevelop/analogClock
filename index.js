add5372b0a67c9ebc187840f3b6bb44a31c02100
cephasDevelop <cephaspc@outlook.com>
2022-12-10 11:02:35 +0300

    comment added

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ function getTime() {
     const singleMove = 6;//  Arm moves by ((360/4)/3)/5 = 6 degree
 
     // DYNAMICALLY SHOWING THE CURRENT TIME IN THE ANALOG CLOCK
-    second.style.transform = `rotate(${singleMove*sec}deg)`;
-    minute.style.transform = `rotate(${singleMove*min + sec/10}deg)`;
+    second.style.transform = `rotate(${singleMove * sec}deg)`;
+    // 60sec = 1min, 1sec = (1/60)sec * 6(deg/sec) ==> every minute has (1/60)*6 degrees of seconds added
+    minute.style.transform = `rotate(${singleMove * min + sec / 10}deg)`;
+    // 1min = (1/60)hr, ==> 30degree*(1/60)hr => 0.5 of theminute shall be added to evry hour
     hour.style.transform = `rotate(${30*hr + min/2}deg)`;
 }
 
